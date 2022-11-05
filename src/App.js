448e7d5dd2a1e6228f9b07eeafa13a03d80c7b41
Mjacke <marshall.jacob16@icloud.com>
2022-11-05 15:38:03 -0600

    Created file structure, started skelleton for website. Path structure complete. Needs actual content, styling, and READme.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import "./style.css";
+import "./App.css";
+import "./index.css";
+
+
+const App = () => <PortfolioContainer />;
+
+export default App;
